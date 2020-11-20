c28d62a1b33de10cec0caa19a81835de719657aa
elppaaa <withbcd@gmail.com>
2020-11-21 03:10:10 +0900

    [feat] 설정 화면 추가
    - 설정 화면을 표시하는 FragmentSettings.java, fragment_settings.xml 작성
    - FragmentSettings 에서는 전체적인 Fragment의 레이아웃을 관리
    - 설정 화면을 관리하고 SharedPreference를 사용하는 FragmentSettingsInner, xml/settings.xml 추가
    	- 설정 화면만 표시하였고, 기능은 구현 에정

--- app/src/main/java/com/buios/buios/FragmentSettings.java
+++ app/src/main/java/com/buios/buios/FragmentSettings.java
@@ -7,19 +7,31 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class FragmentSettings extends Fragment {
+
+  FragmentSettingsInner frag;
+
+  FragmentTransaction transaction;
 
   @Nullable
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
 
-    View rootView =  inflater.inflate(R.layout.fragment_settings, container, false);
+    View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
 
+    transaction = getFragmentManager().beginTransaction();
+
+    frag = FragmentSettingsInner.getInstance();
     // CODE AREA
-
+    transaction.replace(R.id.fragment_settings_framelayout, frag).addToBackStack(null).commit();
 
     return rootView;
   }
+
 }
+
+
+
